c7fe130a1f61383f57cf123f10bfdec46c6cc573
artem <leetim@live.ru>
2018-04-17 18:18:15 +1000

    Изменение структуры

---
+++ robot_IK/Cl_Motor.cpp
@@ -0,0 +1,70 @@
+// Cl_Motor.cpp
+
+#include "Arduino.h"
+#include "Cl_Motor.h"
+
+Cl_Motor::Cl_Motor() { // конструктор
+}
+void Cl_Motor::setup(byte _IN1_pin,  byte _IN2_pin,  byte _EN1_pin,  byte _IN3_pin, byte _IN4_pin, byte _EN2_pin) { // функцию засунуть в setup() программы
+    IN1_pin = 5; // правый вперед
+    IN2_pin = 7; // правый назад
+    IN3_pin = 5; // левый  вперед
+    IN4_pin = 7; // левый назад
+    EN1_pin = 6; // шим правого двигателя
+    EN2_pin = 6; // шим левого двигателя
+  pinMode(IN1_pin, OUTPUT);
+  pinMode(IN2_pin, OUTPUT);
+  pinMode(IN3_pin, OUTPUT);
+  pinMode(IN4_pin, OUTPUT);
+  pinMode(EN1_pin, OUTPUT);
+  pinMode(EN2_pin, OUTPUT);
+  Motor = 0;
+  digitalWrite(IN1_pin, 0);
+  digitalWrite(IN2_pin, 0);
+  digitalWrite(IN3_pin, 0);
+  digitalWrite(IN4_pin, 0);
+}
+void Cl_Motor::loop() { // функцию засунуть в loop() программы
+}
+void Cl_Motor::STOP() { // остановить мотор
+  if (Motor != 0) {
+    Motor = 0;
+    digitalWrite(IN1_pin, 0);
+    digitalWrite(IN2_pin, 0);
+    digitalWrite(IN3_pin, 0);
+    digitalWrite(IN4_pin, 0);
+  }
+}
+void Cl_Motor::FORWARD() { // запустить мотор вперед
+  if (Motor != 1) {
+    Motor = 1;
+    digitalWrite(IN1_pin, 1);
+    digitalWrite(IN2_pin, 0);
+    digitalWrite(IN3_pin, 1);
+    digitalWrite(IN4_pin, 0);
+    analogWrite(EN1_pin, Speed);
+    analogWrite(EN2_pin, Speed + d_S);
+  }
+}
+void Cl_Motor::Back_Left() { // функция движения назад влево
+  if (Motor != 3) {
+    Motor = 3;
+    digitalWrite (IN1_pin, 0);
+    digitalWrite (IN2_pin, 1);
+    digitalWrite (IN3_pin, 0);
+    digitalWrite (IN4_pin, 1);
+    analogWrite(EN1_pin, 160);
+    analogWrite(EN2_pin, 90);
+  }
+}
+void Cl_Motor::Back_Right() { // функция движения назад вправо
+  if (Motor != 4) {
+    Motor = 4;
+    digitalWrite (IN1_pin, 0);
+    digitalWrite (IN2_pin, 1);
+    digitalWrite (IN3_pin, 0);
+    digitalWrite (IN4_pin, 1);
+    analogWrite(EN1_pin, 90);
+    analogWrite(EN2_pin, 160);
+  }
+}
---
+++ robot_IK/robot_IK.ino
@@ -0,0 +1,43 @@
+/*robot_IK.ino
+  #1 мотор
+  #2 датчики
+*/
+//#1 мотор
+#include "Cl_Motor.h"
+Cl_Motor Motor; // создать мотор
+const byte IN1_pin = 5; // определяем пины управления двигателями правый
+const byte IN2_pin = 7; // правый
+const byte IN3_pin = 5; // левый
+const byte IN4_pin = 7; // левый
+const byte EN2_pin = 6; // шим правого двигателя
+const byte EN1_pin = 6; // шим левого двигателя
+
+
+
+//#2 датчики
+#include "Cl_IK.h"
+Cl_IK IK_L, IK_C, IK_R;
+const byte IK_L_pin = 3; // пин левого ик-датчика
+const byte IK_C_pin = 2; // пин центрального ик-датчика
+const byte IK_R_pin = 1; // пин правого датчика
+
+void setup() {
+  //#1 мотор
+  Motor.setup(IN1_pin, IN2_pin, EN1_pin, IN3_pin, IN4_pin, EN2_pin);
+  //#2 датчики
+  IK_L.setup(IK_L_pin);
+  IK_C.setup(IK_C_pin);
+  IK_R.setup(IK_R_pin);
+}
+
+void loop() {
+  //#1 мотор
+  Motor.loop();
+  //#2 датчики
+  IK_L.loop();
+  IK_C.loop();
+  IK_R.loop();
+  if (! IK_L.IK || ! IK_C.IK )  Motor.Back_Right();
+  else if (! IK_R.IK) Motor. Back_Left();
+  else Motor.FORWARD();
+}
